79a49fe06b880805bb31df7247a8bd14fac05dc5
Md. Osman Faruk <programmming.code@gmail.com>
2021-09-07 12:47:01 +0600

    workingOnRegister

--- frontend/src/components/Auth/Register.js
+++ frontend/src/components/Auth/Register.js
@@ -1,11 +1,70 @@
-import React from 'react';
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { Helmet } from "react-helmet";
 
 const Register = () => {
-    return (
-        <div>
-            <h1></h1>
-        </div>
-    );
+  return (
+    <>
+      <Helmet>
+        <title>User Signup</title>
+        <meta name="description" content="User Signup" />
+      </Helmet>
+      <Container>
+        <Row>
+          <Col md={12}>
+            <div className="account">
+              <div className="account__section">
+                <h1>
+                  Create your <span>MERN Blog</span> account{" "}
+                </h1>
+                <form>
+                  <div className="group">
+                    <input
+                      type="text"
+                      name="name"
+                      className="group__control"
+                      placeholder="Name"
+                    />
+                  </div>
+                  <div className="group">
+                    <input
+                      type="text"
+                      name="name"
+                      className="group__control"
+                      placeholder="username"
+                    />
+                  </div>
+                  <div className="group">
+                    <input
+                      type="email"
+                      name="email"
+                      className="group__control"
+                      placeholder="Email"
+                    />
+                  </div>
+                  <div className="group">
+                    <input
+                      type="password"
+                      name="password"
+                      className="group__control"
+                      placeholder="Password"
+                    />
+                  </div>
+                  <div className="group">
+                    <input
+                      type="submit"
+                      className="loginRegisterBtn"
+                      value="Signup"
+                    />
+                  </div>
+                </form>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 };
 
 export default Register;
