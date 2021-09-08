159ea35c55d8e24250efe83afe2b595028d5305c
Md. Osman Faruk <programmming.code@gmail.com>
2021-09-08 23:16:46 +0600

    wroking

--- frontend/src/components/Create/CreatePost.js
+++ frontend/src/components/Create/CreatePost.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Row, FloatingLabel, Form } from "react-bootstrap";
 import { Helmet } from "react-helmet";
 import ReactQuill from "react-quill";
 
@@ -11,26 +11,58 @@ const CreatePost = () => {
         <meta name="description" content="Create New Post" />
       </Helmet>
       ;
-      <div  className="create__post">
-        <Container >
+      <div className="create__post">
+        <Container>
           <Row>
             <Col md={12}>
               <div>
                 <form action="">
                   <h1>Submit your Post</h1>
                   <div className="create__post-card">
-                    <div className="create__post-group"> 
-                    <h2>Post Information</h2>
-                    <div className="textInputGroup">
-                      <label htmlFor="title">Topic Title*</label>
-                      <input
-                        type="text"
-                        name="title"
-                        id="title"
-                        className="textInputGroup__control"
-                        placeholder="Blog Title"
-                      />
+                    <div className="create__post-group">
+                      <h2>Post Information</h2>
+                      <div className="textInputGroup">
+                        <label htmlFor="title">Topic Title*</label>
+                        <input
+                          type="text"
+                          name="title"
+                          id="title"
+                          className="textInputGroup__control"
+                          placeholder="Blog Title"
+                        />
+                      </div>
                     </div>
+                    <div>
+                      <Row>
+                        <Col md={6}>
+                          <div   className="selectGroup">
+                            <label htmlFor="title">Language</label>
+
+                            <Form.Select
+                            
+                              aria-label="Floating label select example"
+                              className="selectGroup-text"
+                            >
+                              <option value="1">English</option>
+                              <option value="2">Bangla</option>
+                            </Form.Select>
+                          </div>
+                        </Col>
+                        <Col md={6}>
+                          <div className="selectGroup">
+                            <label htmlFor="title">Content type</label>
+
+                            <Form.Select
+                              aria-label="Floating label select example"
+                              className="selectGroup-text"
+                            >
+                              <option value="1">One</option>
+                              <option value="2">Two</option>
+                              <option value="3">Three</option>
+                            </Form.Select>
+                          </div>
+                        </Col>
+                      </Row>
                     </div>
                   </div>
                 </form>
