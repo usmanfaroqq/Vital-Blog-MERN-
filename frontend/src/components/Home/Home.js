d04800c59e598c3ef445d85ff7b03a51b5f0ac7a
Md. Osman Faruk <programmming.code@gmail.com>
2021-09-21 09:07:03 +0600

    MakingCardForShowingDataInHome

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Helmet } from "react-helmet";
+import {Container, Row, Col} from "react-bootstrap";
 import HomeAllPosts from "./HomeAllPosts";
+
 
 const Home = () => {
   return (
@@ -9,7 +11,17 @@ const Home = () => {
         <title>Vital Blog</title>
         <meta name="description" content="Learn HTML css js etc" />
       </Helmet>
-      <HomeAllPosts/>
+      <Container>
+        <Row>
+          <Col md={8}>
+            <HomeAllPosts/>
+          </Col>
+          <Col md={4}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem aliquid, consectetur cumque aspernatur obcaecati neque fuga possimus placeat? Voluptate ipsa, repellat porro doloribus explicabo blanditiis reprehenderit! Laudantium vero unde maiores?
+          Aliquam illum sunt odit. Architecto ducimus minima asperiores culpa nam tempora suscipit neque ratione maxime. Repellat, molestias corrupti temporibus error sequi ipsam provident enim eaque, omnis, minima illo consequuntur reprehenderit.
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 };
--- frontend/src/components/Home/HomeAllPosts.js
+++ frontend/src/components/Home/HomeAllPosts.js
@@ -1,30 +1,54 @@
-import React, {useEffect, useState} from 'react';
-import {useSelector, useDispatch} from "react-redux"
-import {allHomePosts} from '../../redux/asyncMethods/HomeGetAllPostMethod';
-import { useParams } from 'react-router-dom';
-
-
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { allHomePosts } from "../../redux/asyncMethods/HomeGetAllPostMethod";
+import { useParams } from "react-router-dom";
+import { Container, Row, Col, Card } from "react-bootstrap";
 
 const HomeAllPosts = () => {
-    let {page} = useParams();
-    if(page === undefined){
-        page = 1
-    }
-    const {loading} = useSelector((state) => state.PostReducer)
-    const {posts, count, perPage} = useSelector(state => state.FetchPosts);
-    const dispatch = useDispatch();
-    useEffect(() => {
-        dispatch(allHomePosts(page))
-    }, [])
-    console.log("page",page)
-    console.log("posts",posts)
-    console.log("count", count)
-    console.log("perPage", perPage)
-    return (
+  let { page } = useParams();
+  if (page === undefined) {
+    page = 1;
+  }
+  const { loading } = useSelector((state) => state.PostReducer);
+  const { posts, count, perPage } = useSelector((state) => state.FetchPosts);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(allHomePosts(page));
+  }, []);
+  console.log("page", page);
+  console.log("posts", posts);
+  console.log("count", count);
+  console.log("perPage", perPage);
+  return (
+    <div>
+      <Container>
         <div>
-            
+          <Row>
+            <Col md={4}>
+              <div>
+                <Card.Img variant="top" src="holder.js/100px180" />
+              </div>
+            </Col>
+            <Col md={8}>
+              <div>
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                </Card.Body>
+              </div>
+            </Col>
+          </Row>
         </div>
-    );
+      </Container>
+    </div>
+  );
 };
 
 export default HomeAllPosts;
