9f04f55addf4a23f4bfb5aeaf9b5bb9e0027509a
Md. Osman Faruk <programmming.code@gmail.com>
2021-09-26 17:02:45 +0600

    added

--- backend/server.js
+++ backend/server.js
@@ -16,9 +16,9 @@ app.use(bodyParser.json());
 app.use(cors())
 
 // // Routes
-// app.get("/", (req, res) => {
-//   res.send("Welcome to vital Blog");
-// }); // Home Route
+app.get("/", (req, res) => {
+  res.send("Welcome to vital Blog");
+}); // Home Route
 
 app.use('/', authRouter) // Register/login Function
 app.use('/', postRouter) // post/get Route
@@ -34,12 +34,12 @@ app.use('/', profileRouter) // post/get Route
 
 // Server configuration
 const PORT = process.env.PORT || 4000;
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, `./../frontend/build/`)));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  })
-}
+// if(process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, `./../frontend/build/`)));
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+//   })
+// }
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
